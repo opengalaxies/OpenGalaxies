7a97de062b9672e852204d3f3a8c209783b5e72e
Dallin Wellington <dallin.wellington@gmail.com>
2009-10-17 18:07:49 -0600

    [Added] compiling.html stub. [Added] create_all.sql and create_db.sql. create_all.sql will construct the tables and stored procedures for a database, while create_db will create a default database the user can use.

---
+++ sql/create_all.sql
@@ -0,0 +1,6 @@
+-- Execute the scripts to construct the database tables, and stored procedures.
+-- Please keep the scripts alphabetized and in their correct categories.
+
+-- Tables
+
+-- Stored Procs
---
+++ sql/create_db.sql
@@ -0,0 +1,4 @@
+-- Drop the existing database if it exists and create a new one.
+DROP DATABASE IF EXISTS OpenGalaxies;
+CREATE DATABASE OpenGalaxies;
+USE OpenGalaxies;
