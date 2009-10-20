a28874701f26cee07fec09d2a5ef4e3d4f6c1e34
Dallin Wellington <dallin.wellington@gmail.com>
2009-10-20 00:13:35 -0600

    [Added] Some .stub files to define the folder structure a bit more. [Added] ogdef.h which contains some common definitions used throughout the project. [Added] ogtypes.h which contains common types used throughout the project. [Modified] Removed the addition of the subdirectory of src inside CMakeLists.txt until we have something that is compilable. [Removed] All CMakeLists.txt from the src folder, until there is something compilable.

---
+++ src/framework/util/ogdef.h
@@ -0,0 +1,30 @@
+/*
+    OpenGalaxies PreCU Server - A Star Wars Galaxies Pre-Combat Upgrade Server.
+    Copyright (C) 2009  OpenGalaxies (www.opengalaxies.org)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef OGDEF_H
+#define OGDEF_h
+
+#include <config.h>
+
+#define SAFE_DELETE( a ) \
+	if(a) { delete a; a = 0; }
+
+#define SAFE_RELEASE( a ) \
+	if(a) { a->Release(); a = 0; }
+
+#endif
---
+++ src/framework/util/ogtypes.h
@@ -0,0 +1,36 @@
+/*
+    OpenGalaxies PreCU Server - A Star Wars Galaxies Pre-Combat Upgrade Server.
+    Copyright (C) 2009  OpenGalaxies (www.opengalaxies.org)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef OGTYPES_H
+#define OGTYPES_H
+
+//
+// Common Redefinitions
+//
+typedef	char					int8;
+typedef short					int16;
+typedef int						int32;
+typedef long long				int64;
+typedef unsigned char			uint8;
+typedef unsigned short			uint16;
+typedef unsigned int			uint32;
+typedef unsigned long long		uint64;
+
+typedef OBJECT_HANDLE			uint64;
+
+#endif
