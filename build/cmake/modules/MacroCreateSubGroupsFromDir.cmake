ccb027b48a4ad80b0884e0978a98e23b510d7e78
Dallin Wellington <dead1ock@opengalaxies.org>
2009-11-07 03:27:02 -0700

    [Combined] the ogutil and ogcore libraries under one ogframework library. All source files that go under the framework folder, will not be included into the ogframework project. This should save us from DLL fatigue. [Added] a macro to gather a list of subdirectories and add them automatically as source groups for MSVC. [Removed] old CMakeLists.txt files that no longer are needed.

---
+++ build/cmake/modules/MacroCreateSubGroupsFromDir.cmake
@@ -0,0 +1,29 @@
+# OpenGalaxies - A Star Wars Galaxies PreCU Server Emulator
+#
+# Copyright (c) 2009 OpenGalaxies <http://www.opengalaxies.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+
+# This file is responsible for checking the endianness of the
+# machine and correctly configure the project to build appropriatly.
+
+MACRO( CREATE_SUBGROUPS_FROM_DIR )
+	FILE( GLOB SUB_DIRS RELATIVE ${CMAKE_CURRENT_SOURCE_DIR} * )
+	foreach( DIR ${SUB_DIRS} )
+		if( IS_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/${DIR} )
+			FILE( GLOB DIR_SOURCES ${CMAKE_CURRENT_SOURCE_DIR}/${DIR}/*.cc )
+			FILE( GLOB DIR_HEADERS ${CMAKE_CURRENT_SOURCE_DIR}/${DIR}/*.h )
+			SOURCE_GROUP( ${DIR} FILES ${DIR_SOURCES} ${DIR_HEADERS} )
+		endif( )
+	endforeach( )
+ENDMACRO()
